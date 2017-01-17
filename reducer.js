c0c2e1b62520957f4502403e6066ad827d325d6b
gabrielamelian <gabrielamelian@gmail.com>
2017-01-18 12:42:36 +1300

    SET_PLAYERS test passes

--- reducer.js
+++ reducer.js
@@ -8,13 +8,10 @@ function reducer(state, action){
       newState.deck = [1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8]
       return newState
 
-      break;
-
     case 'SET_PLAYERS':
       action.payload.forEach(function(playerName, i){
         newState.players[i + 1] = {name: playerName, hand: [], playerPosition: i + 1, immune: false, eliminated: false}
       })
-
       return newState
   }
 }
