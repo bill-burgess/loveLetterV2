80ad9bf0c39de62d27ee10dacbcac887ec1c2d07
gabrielamelian <gabrielamelian@gmail.com>
2017-01-18 12:41:50 +1300

    SET_PLAYERS test passes

--- reducer.js
+++ reducer.js
@@ -1,13 +1,20 @@
 const clone = require('clone')
 
 module.exports = reducer
-
 function reducer(state, action){
+  const newState = clone(state)
   switch (action.type) {
     case 'GENERATE_DECK':
-      const newState = clone(state)
       newState.deck = [1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8]
       return newState
 
+      break;
+
+    case 'SET_PLAYERS':
+      action.payload.forEach(function(playerName, i){
+        newState.players[i + 1] = {name: playerName, hand: [], playerPosition: i + 1, immune: false, eliminated: false}
+      })
+
+      return newState
   }
 }
--- test/startGame.test.js
+++ test/startGame.test.js
@@ -15,7 +15,7 @@ test('GENERATE_DECK | returns a full deck of 16 cards', t => {
       7: {name: 'countess', rank: 7},
       8: {name: 'princess', rank: 8},
     },
-    players: [],
+    players: {},
     deck: [],
     playerTurn: null
   }
@@ -36,7 +36,7 @@ test('GENERATE_DECK | returns a full deck of 16 cards', t => {
       7: {name: 'countess', rank: 7},
       8: {name: 'princess', rank: 8},
     },
-    players: [],
+    players: {},
     deck: [1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8],
     playerTurn: null
   }
@@ -45,5 +45,56 @@ test('GENERATE_DECK | returns a full deck of 16 cards', t => {
 
   t.deepEqual(newState, expectedState, 'should give an array of card ranks')
 
+  t.end()
+})
+
+test('SET_PLAYERS | adds an object to the object players in the state for each player name submitted', t => {
+
+  const state = {
+    cardTypes: {
+      1: {name: 'guard', rank: 1},
+      2: {name: 'priest', rank: 2},
+      3: {name: 'baron', rank: 3},
+      4: {name: 'handmaid', rank: 4},
+      5: {name: 'prince', rank: 5},
+      6: {name: 'king', rank: 6},
+      7: {name: 'countess', rank: 7},
+      8: {name: 'princess', rank: 8},
+    },
+    players: {},
+    deck: [1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8],
+    playerTurn: null
+  }
+  freeze(state)
+
+  const action = {
+    type: 'SET_PLAYERS',
+    payload: ['Bill', 'Tom', 'Dick', 'Harry']
+  }
+
+  const expectedState = {
+    cardTypes: {
+      1: {name: 'guard', rank: 1},
+      2: {name: 'priest', rank: 2},
+      3: {name: 'baron', rank: 3},
+      4: {name: 'handmaid', rank: 4},
+      5: {name: 'prince', rank: 5},
+      6: {name: 'king', rank: 6},
+      7: {name: 'countess', rank: 7},
+      8: {name: 'princess', rank: 8},
+    },
+    players: {
+      1: {name: 'Bill', hand: [], playerPosition: 1, immune: false, eliminated: false},
+      2: {name: 'Tom', hand: [], playerPosition: 2, immune: false, eliminated: false},
+      3: {name: 'Dick', hand: [], playerPosition: 3, immune: false, eliminated: false},
+      4: {name: 'Harry', hand: [], playerPosition: 4, immune: false, eliminated: false}
+    },
+    deck: [1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8],
+    playerTurn: null
+  }
+
+  const newState = reducer(state, action)
+
+  t.deepEqual(newState, expectedState, 'should add 4 players to the players object')
   t.end()
 })
